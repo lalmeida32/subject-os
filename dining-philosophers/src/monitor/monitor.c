537c944e83a02d1a920973f08a21690cb5aa9787
Lucas Ferreira de Almeida <lucasf.almeida.dev@gmail.com>
2022-11-06 04:26:22 -0300

    chore: monitor creation and testing

---
+++ dining-philosophers/src/monitor/monitor.c
@@ -0,0 +1,40 @@
+#include "monitor.h"
+#include <stdlib.h>
+#include <pthread.h>
+
+struct monitor {
+  int n_functions;
+  void *shared_data;
+  void *(**functions)(void *, void *);
+  pthread_mutex_t *mutex;
+};
+
+monitor_t *monitor_init(int n_functions, void *shared_data, void *(**functions)(void *, void *)) {
+  monitor_t *m = malloc(sizeof(monitor_t));
+  m->n_functions = n_functions;
+  m->shared_data = shared_data;
+  m->functions = functions;
+  m->mutex = malloc(sizeof(pthread_mutex_t));
+  pthread_mutex_init(m->mutex, NULL);
+  return m;
+}
+
+void monitor_destroy(monitor_t **m, void (*shared_data_destroy)(void *)) {
+  if (shared_data_destroy == NULL)
+    free((*m)->shared_data);
+  else
+    shared_data_destroy((*m)->shared_data);
+
+  free((*m)->functions);
+  pthread_mutex_destroy((*m)->mutex);
+  free((*m)->mutex);
+  free(*m);
+  *m = NULL;
+}
+
+void *monitor_exec(monitor_t *m, int function_id, void *arg) {
+  pthread_mutex_lock(m->mutex);
+  void *r = m->functions[function_id](m->shared_data, arg);
+  pthread_mutex_unlock(m->mutex);
+  return r;
+}
---
+++ dining-philosophers/src/monitor/monitor.h
@@ -0,0 +1,21 @@
+#ifndef _MONITOR_H_
+#define _MONITOR_H_
+
+typedef struct monitor monitor_t;
+
+// This function initializes a monitor structure.
+// It receives a function array and data to be shared between this functions.
+// The shared data must be in the heap memory, in order to work with threads.
+monitor_t *monitor_init(int n_functions, void *shared_data, void *(**functions)(void *, void *));
+
+// This function free all memory allocated by the monitor structure.
+// Optionally receives a function to free shared_data memory. Otherwise uses
+//  stdlib's free function.
+void monitor_destroy(monitor_t **m, void (*shared_data_destroy)(void *));
+
+// This function properly executes a function stored in the monitor structure.
+// The monitor structure guarantees the mutual exclusion of these functions by
+//  using an internal mutex.
+void *monitor_exec(monitor_t *m, int function_id, void *arg);
+
+#endif
