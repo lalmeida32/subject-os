516f4cea825f52651a0d69f1909b212baa0861ea
Lucas Ferreira de Almeida <lucasf.almeida.dev@gmail.com>
2022-11-08 19:54:00 -0300

    docs: adding relevant commentaries to dinphil module

--- dining-philosophers/src/dinphil/dinphil.c
+++ dining-philosophers/src/dinphil/dinphil.c
@@ -15,9 +15,9 @@ typedef enum {
 struct dinphil_shared_data {
   dinphil_state_t state[N_THREADS];
   pthread_cond_t self[N_THREADS];
-  bool_t priority[N_THREADS];
-  int counter[N_THREADS];
-  int counter_eat[N_THREADS];
+  bool_t priority[N_THREADS]; // Criteria: counter is equal to counter_max
+  int counter[N_THREADS];     // Philosopher tried to eat but failed
+  int counter_eat[N_THREADS]; // How many times philosopher ate
   int counter_max;
   pthread_mutex_t *mutex;
 };
@@ -45,13 +45,18 @@ static void _print_state(dinphil_shared_data_t *shared_data) {
 }
 
 static void _test(dinphil_shared_data_t *shared_data, int i) {
+  // If at least one of the adjacent philosophers is priority, then the current
+  //  philosopher cannot eat or try to eat. Must wait that adjacent philosopher
+  //  eat.
   bool_t is_blocked = shared_data->priority[_left(i)] || shared_data->priority[_right(i)];
 
+  // If can eat
   if ((shared_data->state[_left(i)] != EATING) &&
       (shared_data->state[i] == HUNGRY) &&
       (shared_data->state[_right(i)] != EATING) &&
       !is_blocked) {
     
+    // Then eat
     shared_data->state[i] = EATING;
     shared_data->counter[i] = 0;
     shared_data->priority[i] = FALSE;
@@ -60,9 +65,13 @@ static void _test(dinphil_shared_data_t *shared_data, int i) {
 
     pthread_cond_signal(&shared_data->self[i]);
   }
+
+  // If tried to eat
   else if (shared_data->state[i] == HUNGRY && !is_blocked) {
     int counter = ++shared_data->counter[i];
     printf("\nPhilosopher %d tried to eat %d times\n", i, counter);
+
+    // If get priority criteria
     if (counter == shared_data->counter_max) {
       shared_data->priority[i] = TRUE;
       _print_state(shared_data);
@@ -78,7 +87,10 @@ void *dinphil_pickup(void *sd, void *args) {
   shared_data->state[i] = HUNGRY;
   _print_state(shared_data);
 
+  // Try to eat
   _test(shared_data, i);
+
+  // Could not eat
   if (shared_data->state[i] != EATING)
     pthread_cond_wait(&shared_data->self[i], mutex);
   
@@ -89,8 +101,11 @@ void *dinphil_putdown(void *sd, void *args) {
   dinphil_shared_data_t *shared_data = (dinphil_shared_data_t *) sd;
   int i = *(int *) args;
 
+
   shared_data->state[i] = THINKING;
   _print_state(shared_data);
+  
+  // Notify adjacent philosophers
   _test(shared_data, _left(i));
   _test(shared_data, _right(i));
   
@@ -103,6 +118,7 @@ dinphil_shared_data_t *dinphil_shared_data_init() {
   shared_data->counter_max = DEFAULT_COUNTER_MAX;
 
   for (int i = 0; i < N_THREADS; i++) {
+    // Initialization of each philosopher.
     shared_data->state[i] = THINKING;
     shared_data->counter[i] = 0;
     shared_data->priority[i] = FALSE;
--- dining-philosophers/src/dinphil/dinphil.h
+++ dining-philosophers/src/dinphil/dinphil.h
@@ -20,6 +20,12 @@ dinphil_shared_data_t *dinphil_shared_data_init();
 // This is useful to get access to the mutex of the monitor
 void dinphil_set_mutex(dinphil_shared_data_t *shared_data, pthread_mutex_t *mutex);
 
+// This function just destroy the shared data and is useful to properly freeing
+//  memory inside a monitor.
 void dinphil_shared_data_destroy(void *sd);
+
+// This is useful to set how many times a philosopher must try to eat to get
+//  priority.
+void dinphil_set_counter_max(dinphil_shared_data_t *shared_data, int counter_max);
 
 #endif
